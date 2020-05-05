3be90ac066bc240310372b965207a0b86e4531ea
morelandjo <morlanjo@gmail.com>
2020-05-05 10:20:29 -0400

    array fix

--- classes/tests.php
+++ classes/tests.php
@@ -87,7 +87,8 @@ class tests {
 	 */
 	public function registerTests($testType, $tests) {
 		foreach ($tests as $test) {
-			array_push($this->testTypes,$testType);
+			$type = $this->testTypes;
+			array_push($type,$testType);
 			$callable = array($type, '_'.$test);
 			array_push($this->tests,array(
 				'type'		=> $testType,
